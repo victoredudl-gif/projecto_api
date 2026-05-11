dfd641a77138d0676e1ff33309e9fbc5ca228be4
Vitor Dominguez <victoredudl@gmail.com>
2026-05-11 08:46:33 -0400

    add: creacion de crud de producto con ModelViewset

--- projecto_api/projecto_api/settings.py
+++ projecto_api/projecto_api/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
+    'rest_framework',
+    'inventario',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,13 @@ WSGI_APPLICATION = 'projecto_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'bd_inventario',
+        'USER': 'postgres',
+        'PASSWORD': '3031vctoria',
+        'HOST':'localhost',
+        'PORT': '5432',
+
     }
 }
 
